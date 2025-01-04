3f77516f2f6f2326cc275b241a74c66f63f5cefc
tashinoki <kenya.11263@gmail.com>
2025-01-04 23:56:48 +0900

    add sample code for retry policy

---
+++ source/KinoToyBox/KinoToyBoxFunctions/DurableRetryTest.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+
+namespace KinoToyBoxFunctions;
+
+public class DurableRetryTest
+{
+    [FunctionName(nameof(TestOrchestratorStartAsync))]
+    public async Task TestOrchestratorStartAsync(
+        [ServiceBusTrigger("durable-retry-test")] string myQueueItem,
+        [DurableClient] IDurableOrchestrationClient orchestrationClient,
+        ILogger log,
+        CancellationToken cancellationToken)
+    {
+        await orchestrationClient.StartNewAsync(nameof(TestOrchestrator));
+    }
+
+    [FunctionName(nameof(TestOrchestrator))]
+    public async Task TestOrchestrator(
+        [OrchestrationTrigger] IDurableOrchestrationContext orchestrationContext,
+        ILogger log,
+        CancellationToken cancellationToken)
+    {
+        // サブオーケストレータの呼び出し
+        await orchestrationContext.CallSubOrchestratorWithRetryAsync(nameof(TestSubOrchestrator), new RetryOptions(TimeSpan.FromSeconds(1), 5)
+        {
+            RetryTimeout = TimeSpan.FromMinutes(1)
+        }, cancellationToken);
+
+        // アクティビティの呼び出し
+        await orchestrationContext.CallActivityWithRetryAsync(nameof(TestActivityAsync), new RetryOptions(TimeSpan.FromSeconds(5), 10)
+        {
+            RetryTimeout = TimeSpan.FromMinutes(5),
+        }, cancellationToken);
+    }
+
+    [FunctionName(nameof(TestSubOrchestrator))]
+    public Task TestSubOrchestrator(
+        [OrchestrationTrigger] IDurableOrchestrationContext oeDurableOrchestrationContext,
+        ILogger log,
+        CancellationToken cancellationToken)
+    {
+        // 例外が起きるかもしれない処理
+        // ...
+
+        return Task.CompletedTask;
+    }
+
+    [FunctionName(nameof(TestActivityAsync))]
+    public Task TestActivityAsync(
+        [ActivityTrigger] IDurableActivityContext activityContext,
+        ILogger log,
+        CancellationToken cancellationToken)
+    {
+        // 例外が起きるかもしれない処理
+        // ....
+
+        return Task.CompletedTask;
+    }
+}
+
